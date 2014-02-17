994aa76cc562f853e6e5ae6611d4e3fc7cba4421
fabrixxm <fabrix.xm@gmail.com>
2014-02-17 15:55:38 +0100

    add view raw files

--- git-serve.py
+++ git-serve.py
@@ -49,10 +49,12 @@ class GIT:
 		else:
 			dirset = set(dirs)
 		
-		files = [ b.strip("\n\r") for b in files.split("\n") if b not in dirset ]
+		files = [ b.strip("\n\r") for b in files.split("\n") if b not in dirset and b.strip("\n\r")!="" ]
 		
 		return sorted(dirs), sorted(files)
-	
+	@classmethod
+	def show(cls, file, ref="HEAD"):
+		return cls._do("show", ref+":"+file)
 
 class GITServePages(object):
 	def __init__(self):
@@ -60,6 +62,7 @@ class GITServePages(object):
 		self.routes = {
 			re.compile(r'^/$') : self.index,
 			re.compile(r'^/browse(?P<path>.*)$') : self.browse,
+			re.compile(r'^/view(?P<path>.*)$') : self.view,
 		}
 		
 	def route(self, request):
@@ -89,12 +92,15 @@ class GITServePages(object):
 		return None
 	
 	def _tpl(self, text):
-		style="body{padding:0px;margin:0px;font-family:sans-serif}article{width:90%;margin:0px auto}header{padding:20px 5%;background-color:#404e61;color:#fff}header>a{color:#DDD}header>a:hover{color:#fff}footer{padding:10px 5%;background-color:#152a47;color:#fff}"
+		style="""body{padding:0px;margin:0px;font-family:sans-serif}article{width:90%;margin:0px auto}header{padding:20px 5%;background-color:#404e61;color:#fff}header>a{color:#DDD}header>a:hover{color:#fff}footer{padding:10px 5%;background-color:#152a47;color:#fff}
+		li.dir{list-style-image:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAABTVBMVEUAABRISEhJSUlGTVNMTExRUVFWVlZbW1tfX180ZaQ0ZaU0ZqQ1ZqQ1ZqU2ZqQ2ZqU2Z6U3Z6U4Z6Q3aKY4aKU5aKU6aaVlZWVBbaZqampsbGxubm5FeLJzc3N0dHR4eHh5eXl6enpQg7qAgIB4hpeMjIyNjY1tnM5unM5wns+ZmZmbm5t4o9J5pNN6pNN6pdF+ptOhoaF+p9SioqKBqNWkpKSlpaSlpaWEq9WFq9Wnp6eHrdepqamJrtiqqqqrq6uLsNiLsNmsrKytra2OstmOstqurq6Qs9qRtNqRtNuwsLCRtduStduVttyUt9yVt9yzs7OWuNy0tLSZud2Zut2but22tracut23t7e5ubm7u7u9vb2pxOLBwcDExMTFxcWxyeXHx8fJycm2zea3z+e4z+e4z+i+0um+0+m+0+q+1Oq/1OrB1erE1+vG2Ow1AMXeAAAAAXRSTlMAQObYZgAAALtJREFUGNNjYEAHigpggBCQT0qMj4sLkoMLyMZ7OdvbOampqaqqKksBBaRjneyszE1NncIio4wlgQKS0RL8IMADBJycnMIM4hG8mdnZWVlZGSCQxsXAEcCT4+np7e0DAoEp3AzsbgK5fjDgkczLwGbDlxcCAf6ujqm8DKxmgumhwb4+7i621tYOCbwMLIYiMSZ6UGAUzsvAqC1kqaEOBZoWvAxMWqIGOjCgqw9UoSKmhAAy/AzMnMiAhwEAATQqrYcDKI4AAAAASUVORK5CYII=);}
+		li.file{list-style-image:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAP1BMVEUAAACBgYGVlZWZmZnExMTFxcXGxsbHx8fIyMjq6urr6+vs7Ozt7eXt7ebt7e3u7u7v7+/w8PDx8fHy8vL///9IyRz5AAAAAXRSTlMAQObYZgAAAGdJREFUGNNlj0sWgCAMA0FFPtUCxfufVahQeZrlTLKIWvUUVaOvKZoBeB9CAMDcgd+M2WusgKYBMQ4Q2E8N3uMBb6PpGE8BvI8pJQFtb51zAthnIuoAH09UBsBWyFQG+HxZ5reL+ucG2iMI0Xh/di8AAAAASUVORK5CYII=);}
+		"""
 		return """<!DOCTYPE html><head><title>{repo_name}</title>
 		<style>{style}</style>
 		</head>
 		<body>
-		<header><strong>{repo_name}</strong> - <a href="/browse/">browse</a></header>
+		<header><strong>{repo_name}</strong> - <a href="/">home</a> - <a href="/browse/">browse</a></header>
 		<article>{content}</article>
 		<footer>clone this repo: <code>git clone http://{host}:{port}{repo_name}</code></footer>
 		</body>
@@ -125,17 +131,28 @@ class GITServePages(object):
 		if path!="":
 			path+="/"
 		dirs, files = GIT.files(path)
+		if len(dirs)==0 and len(files)==0:
+			return None
 		if path!="":
 			dirs = [path+".."] + dirs
-		txt_browse = "<ul>"
-		print path, dirs, files
+		txt_browse = "<h3>"+path+"</h3>"
+		txt_browse += "<ul>"
 		for name in dirs:
-			txt_browse += "<li class='dir'><a href='/browse/{0}'>{0}</a></li>".format(name)
+			name = name.replace(path,"")
+			txt_browse += "<li class='dir'><a href='/browse/{0}{1}'>{1}</a></li>".format(path,name)
 		for name in files:
-			txt_browse += "<li class='file'>{0}</li>".format(name)
+			name = name.replace(path,"")
+			txt_browse += "<li class='file'><a href='/view/{0}{1}'>{1}</a></li>".format(path,name)
 		txt_browse += "</ul>"
 		return (200, "text/html", self._tpl(txt_browse))
-		
+	
+	def view(self, path):
+		try:
+			text = GIT.show(path.strip("/"))
+		except CalledProcessError:
+			return None
+		text = "<pre>{0}</pre>".format(text)
+		return (200, "text/html", self._tpl(text))
  
 class GITRequestHandler(CGIHTTPServer.CGIHTTPRequestHandler):
 	def translate_path(self, path):
@@ -189,6 +206,7 @@ def start_serve(git_repo_path, port=8001):
 
 if __name__=="__main__":	
 	import sys
+	
 	
 	port = 8001
 	if len(sys.argv)>1 and sys.argv[1].isdigit():
